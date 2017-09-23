f270a21e250e96e0c0d7e468a843fed35e1f55e0
ProfitWarning <christian.fastner@arcor.de>
2017-09-23 23:16:07 +0200

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import * as WebFontLoader from 'webfontloader';
 
 import Boot from './states/boot';
 import Preloader from './states/preloader';
-import Title from './states/title';
 import Pong from './states/pong';
 import * as Utils from './utils/utils';
 import * as Assets from './assets';
@@ -19,8 +18,8 @@ class App extends Phaser.Game {
 
         this.state.add('boot', Boot);
         this.state.add('preloader', Preloader);
-        this.state.add('title', Title);
         this.state.add('pong', Pong);
+
         this.state.start('boot');
     }
 }
